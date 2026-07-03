edc5ef82a2d8961f97e8f8461af01414b1644ad1
Ian SR <iann.ipn9@icloud.com>
2026-07-02 18:50:34 -0600

    init springerror project

---
+++ springerror/src/main/java/com/iappsan/springerror/SpringerrorApplication.java
@@ -0,0 +1,13 @@
+package com.iappsan.springerror;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringerrorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringerrorApplication.class, args);
+	}
+
+}
---
+++ springerror/src/test/java/com/iappsan/springerror/SpringerrorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iappsan.springerror;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringerrorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
